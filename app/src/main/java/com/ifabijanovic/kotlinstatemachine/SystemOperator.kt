0191e0694c717a6941ed75cf6c2f13603fb714ef
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2017-05-09 18:02:32 +0200

    Updates `system` operator.

--- app/src/main/java/com/ifabijanovic/kotlinstatemachine/SystemOperator.kt
+++ app/src/main/java/com/ifabijanovic/kotlinstatemachine/SystemOperator.kt
@@ -2,28 +2,45 @@ package com.ifabijanovic.kotlinstatemachine
 
 import io.reactivex.Observable
 import io.reactivex.Scheduler
+import io.reactivex.rxjava2.traits.*
 import io.reactivex.subjects.ReplaySubject
 
 /**
  * Created by Ivan Fabijanovic on 06/05/2017.
  */
 
-fun <R, Element> system(
-        initialState: R,
-        accumulator: (R, Element) -> R,
+fun <State, Event> Observable<Any>.system(
+        initialState: State,
+        accumulator: (State, Event) -> State,
         scheduler: Scheduler,
-        vararg feedback: (Observable<R>) -> Observable<Element>
-): Observable<R> {
+        vararg feedback: (Observable<State>) -> Observable<Event>
+): Observable<State> {
     return Observable.defer {
-        val replaySubject: ReplaySubject<R> = ReplaySubject.createWithSize(1)
+        val replaySubject: ReplaySubject<State> = ReplaySubject.createWithSize(1)
 
-        val inputs: Observable<Element> = Observable
+        val inputs: Observable<Event> = Observable
                 .merge(feedback.map { it(replaySubject) })
                 .observeOn(scheduler)
 
         return@defer inputs
                 .scan(initialState, accumulator)
-                .startWith(initialState)
+                .doOnNext { replaySubject.onNext(it) }
+    }
+}
+
+fun <State, Event> SharedSequence.Companion.system(
+        initialState: State,
+        accumulator: (State, Event) -> State,
+        vararg feedback: (Driver<State>) -> Driver<Event>
+): Driver<State> {
+    return Driver.defer {
+        val replaySubject: ReplaySubject<State> = ReplaySubject.createWithSize(1)
+
+        val inputs: Driver<Event> = Driver
+                .merge(feedback.map { it(replaySubject.asDriverIgnoreError()) })
+
+        return@defer inputs
+                .scan(initialState, accumulator)
                 .doOnNext { replaySubject.onNext(it) }
     }
 }
